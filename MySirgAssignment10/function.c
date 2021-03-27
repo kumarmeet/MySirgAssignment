7304c4a9b4571f206bba03d6cc2b9d666c5f527f
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-03-27 12:12:33 +0530

    First attempt
    solved 11 out of 16 problems

---
+++ MySirgAssignment10/function.c
@@ -0,0 +1,115 @@
+int sum_of_square_natural_number(int n)
+{
+    int sum = 0;
+
+    if(n == 0)
+        return 0;
+
+    sum = n * n + sum_of_square_natural_number(n - 1);
+    return sum;
+}
+
+int sum_n_odd_natural_number(int n)
+{
+    int sum = 0;
+
+    if(n % 2 == 0)
+        n = n - 1;
+
+    if(n < 0)
+        return 0;
+
+    sum = n + sum_n_odd_natural_number(n - 2);
+    return sum;
+}
+
+int sum_n_even_natural_number(int n)
+{
+    int sum = 0;
+
+    if(n % 2 == 1)
+        n = n - 1;
+
+    if(n < 0)
+        return 0;
+
+    sum = n + sum_n_even_natural_number(n - 2);
+    return sum;
+}
+
+int sum_n_natural_number(int n)
+{
+    int sum = 0;
+
+    if(n == 1)
+        return 1;
+
+    sum = n + sum_n_natural_number(n - 1);
+    return sum;
+}
+
+int reverse_odd_recursion(int n)
+{
+    if(n % 2 == 0)
+        n = n - 1;
+
+    if(n <= 0)
+        return 0;
+
+    printf("%d\t", n);
+    reverse_odd_recursion(n - 2);
+}
+
+int odd_recursion(int n)
+{
+    if(n % 2 == 0)
+        n = n - 1;
+
+    if(n <= 0)
+        return 0;
+
+    odd_recursion(n - 2);
+    printf("%d\t", n);
+}
+
+int reverse_even_recursion(int n)
+{
+    if(n % 2 == 1)
+        n = n - 1;
+
+    if(n <= 0)
+        return 0;
+
+    printf("%d\t", n);
+    even_recursion(n - 2);
+}
+
+int even_recursion(int n)
+{
+    if(n % 2 == 1)
+        n = n - 1;
+
+    if(n <= 0)
+        return 0;
+
+    even_recursion(n - 2);
+    printf("%d\t", n);
+}
+
+void first_n_reverse_recursion(int n)
+{
+  if(n > 0)
+  {
+    printf("%d\n",n);
+    first_n_reverse_recursion(n-1);
+  }
+}
+
+void first_n_recursion(int n)
+{
+  if(n > 0)
+  {
+    first_n_recursion(n-1);
+    printf("%d\n",n);
+  }
+}
---
+++ MySirgAssignment10/main.c.save
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+void first_n_recursion(int);
+void first_n_reverse_recursion(int);
+void first_n_even_natural_recursion(int);
+
+int main()
+{
+
+    return 0;
+}
+
+
+
